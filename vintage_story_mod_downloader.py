2c259aeaaeb26be38016f1aec60b69659080d74d
Lauri Lehtonen <laanle@utu.fi>
2022-09-28 20:13:40 +0300

    Extracted functions from script, rewrote script using functions.

--- vintage_story_mod_downloader.py
+++ vintage_story_mod_downloader.py
@@ -3,6 +3,7 @@ import re
 import os, sys
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
+import logging
 try:
     import wget
 except ImportError:
@@ -10,126 +11,129 @@ except ImportError:
 
 home_url = "https://mods.vintagestory.at"
 modlist_url = "https://mods.vintagestory.at/list/mod"
+OUTPUT_DIR = "output"
 
-req_modname=requests.get(modlist_url,'html.parser')
-search_active = False
-curr_path=os.path.dirname(os.path.abspath(__file__))
+def get_mod_download_url(mod_page_url):
+    req_filelist = requests.get(mod_page_url, 'html.parser')
+    html_filelist = req_filelist.text
 
-config_file = Path("modsdir.txt")
+    p_htmlfilelist = bs(html_filelist, features="lxml")
+    trs = p_htmlfilelist.body.find_all('tr', attrs={"data-assetid": True})
+    latest = max(trs, key=lambda tr: int(tr.attrs["data-assetid"]))
+    fileid = latest.find('a', {'href': re.compile(r'/download\?fileid.')})['href']
+    downlink = home_url + fileid
+    logging.debug("Mod download link:", downlink)
+    return downlink
 
-try:
-    with open(config_file, "r") as f:
-        mods_dir = f.read()
-except FileNotFoundError:
-    mods_dir = input('What is your mod directory?')
-    print(f"Writing {mods_dir} to " + str(config_file.absolute()))
-    with open(config_file, "w") as f:
-        f.write(mods_dir)
 
-modpath = Path(mods_dir)
-
-path_out = Path("output")
-if not path_out.is_dir():
-    print("Creating output directory at " + str(path_out.absolute()))
-    path_out.mkdir()
-
-modlist = os.listdir(modpath)
-mods=[]
-
-search=input("\nDo you wanna search mods by hand? (y or n): ")
-
-if search.lower() not in ['y','yes']:
+def get_mods_from_moddir(modpath):
+    modlist = os.listdir(modpath)
     for mod in modlist:
-        if mod[-4:-1]+mod[-1] == '.zip':
-            upper=[]
-            for j,mm in enumerate(mod):
+        if mod[-4:-1] + mod[-1] == '.zip':
+            upper = []
+            for j, mm in enumerate(mod):
                 if mm.isupper():
                     upper.append(j)
-            modx=mod.split(' ')[0]
+            modx = mod.split(' ')[0]
             if 5 in upper or 6 in upper:
-                m=mod[0:-4]
-                m=m[0:9]
-                mods.append([m,mod[0:-4]])
-                modx=''
-                mody=''
+                m = mod[0:-4]
+                m = m[0:9]
+                mods.append([m, mod[0:-4]])
+                modx = ''
+                mody = ''
             else:
-                m=mod[0:-4]
-                m=m[0:6]
+                m = mod[0:-4]
+                m = m[0:6]
                 if 'xlib' in m:
-                    m='xlib'  
-                mods.append([m,mod[0:-4]])
-else:
-    search_active = True
+                    m = 'xlib'
+                mods.append([m, mod[0:-4]])
+    return mods
 
-if search_active:
-    for i in range(0,1000):
-        mods.append(['',''])
+def get_mod_page_urls(modname):
+    modname = modname.strip(" _-.'!0")
+    regex = r''
+    for m in modname:
+        regex = regex + '[\_\-\.\'\!\s]*' + m
+    regex = regex + '[\_\-\.\'\!\s]*'
+    logging.debug("regex: ", regex)
 
-for m,full_mod in mods:
-    break_ = False
-    mod_name=m
-    if search_active:
-        mod_name=input('\n\nSearch for mod name (0 for quit): ')
-        full_mod=''
-        if mod_name=='0':
-            print("Exiting...")
-            sys.exit(0)
-    
-    mod_name=mod_name.strip(" _-.'!0")
-    regex=r'' 
-    for m in mod_name:
-        regex=regex+'[\_\-\.\'\!\s]*'+m
-    regex=regex+'[\_\-\.\'\!\s]*'
-    #print(regex)
-    
-    html_modname=req_modname.text
-    html_modname=html_modname.strip(" _-.'!")
-    p_htmlmodname=bs(html_modname, features="lxml")
-    #print(p_htmlmodname)
+    html_modname = req_modname.text
+    html_modname = html_modname.strip(" _-.'!")
+    p_htmlmodname = bs(html_modname, features="lxml")
+    # print(p_htmlmodname)
+    urls = p_htmlmodname.find_all('strong', text=re.compile(".*({}).*".format(regex), re.IGNORECASE))
+    return urls
 
-    mod_urls=p_htmlmodname.find_all('strong',text=re.compile(".*({}).*".format(regex),re.IGNORECASE))
-    if len(mod_urls) == 0:
-        break_ = True
-        print("\n\nNo results for {0}\n".format(mod_name))
-    elif len(mod_urls)==1:
-        mod_urls.append('x')
-
-    #os.system('cls')
-        
-    if not break_:
-
-        if mod_urls[-1]!='x':
-            print("\n\nFound more than one mod for search {0}".format(mod_name))
-            if full_mod:
-                print("Full mod name: {0}\n".format(full_mod))
-            for i,n in enumerate(mod_urls):
-                print(str(i+1)+'. '+str(n)[8:-9])
-            x=int(input("\nWhich do you want to download? (0 for pass): "))-1
-            full_mod=str(mod_urls[x])[8:-9]
-            if x==-1:
-               continue
-            mod_url=mod_urls[x].parent['href']
+if __name__ == "__main__":
+    mods = []
+    # Ask user how to download mods
+    while True:
+        answer = int(input("\nSearch mods by hand (1), read mods automatically from a directory (2) , or exit (0)?"))
+        if answer == 1:
+            search_active = True
+            break
+        elif answer == 2:
+            search_active = False
+            config_file = Path("modsdir.txt")
+            try:
+                with open(config_file, "r") as f:
+                    mods_dir = f.read()
+            except FileNotFoundError:
+                mods_dir = input('What is your mod directory?')
+                print(f"Writing {mods_dir} to " + str(config_file.absolute()))
+                with open(config_file, "w") as f:
+                    f.write(mods_dir)
+            modpath = Path(mods_dir)
+            mods = get_mods_from_moddir(modpath)
+            break
+        elif answer == 0:
+            search_active = False
+            break
         else:
-            mod_url=mod_urls[0].parent['href']
+            print("Input not recognized. ")
+
+    # Get page with all the mods
+    req_modname = requests.get(modlist_url, 'html.parser')
+
+    # Create output directory
+    path_out = Path(OUTPUT_DIR)
+    if not path_out.is_dir():
+        print("Creating output directory at " + str(path_out.absolute()))
+        path_out.mkdir()
+
+    while len(mods) > 0 or search_active:
+        if search_active:
+            mod_name = input('\n\nSearch for mod name (0 for quit): ')
+            full_mod = mod_name
+            if mod_name == '0':
+                break
+        else:
+            mod_name, full_mod = mods.pop()
+
+        mod_urls = get_mod_page_urls(mod_name)
+        if len(mod_urls) == 1:
+            mod_url = mod_urls[0].parent['href']
+        elif len(mod_urls) > 1:
+            print(f"\n\nFound more than one mod for search {mod_name}")
+            if full_mod:
+                print(f"Full mod name: {full_mod}\n")
+            for i, n in enumerate(mod_urls):
+                print(str(i + 1) + '. ' + str(n)[8:-9])
+            x = int(input("\nWhich do you want to download? (0 for pass): ")) - 1
+            full_mod = str(mod_urls[x])[8:-9]
+            if x == -1:
+                continue
+            mod_url = mod_urls[x].parent['href']
+        else:
+            print(f"No results for {mod_name}.")
+            continue
 
         url=home_url+mod_url
+        print(f"Downloading {full_mod}.")
+        download_url = get_mod_download_url(url)
+        response = wget.download(download_url, out=str(path_out))
+        print(f"{full_mod} downloaded successfully.")
 
-        req_filelist = requests.get(url, 'html.parser')
-        html_filelist=req_filelist.text
-        
-        p_htmlfilelist= bs(html_filelist, features="lxml")
-        trs = p_htmlfilelist.body.find_all('tr', attrs={"data-assetid":True})
-        latest = max(trs, key = lambda tr: int(tr.attrs["data-assetid"]))
-        fileid = latest.find('a', {'href':re.compile(r'/download\?fileid.')})['href']
-        downlink=home_url+fileid
+    print("Done. Exiting...")
 
-        #os.system('cls')
-        print('\n\n'+full_mod)
-        print(url)
-        print(downlink)
-        response=wget.download(downlink, out=str(path_out))
-    else:
-        break_=False
-
-c=input("\n\nDownload complete press any key to exit\n")
 
